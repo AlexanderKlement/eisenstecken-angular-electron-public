e2ccfeeeca465ed00e1d5633e69e3bbe8afd0cbf
Ivan Waldboth <csaq5507@uibk.ac.at>
2022-06-29 14:09:44 +0200

    Removed useless stuff

--- src/app/shared/services/listener.service.ts
+++ src/app/shared/services/listener.service.ts
@@ -2,34 +2,28 @@ import {Injectable, Renderer2, RendererFactory2} from '@angular/core';
 import {NavigationService} from './navigation.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ListenerService {
 
-    renderer: Renderer2;
-
-  private history: string[] = [];
+  renderer: Renderer2;
 
 
   constructor(private rendererFactory2: RendererFactory2, private navigation: NavigationService) {
-        this.renderer = this.rendererFactory2.createRenderer(null, null);
-        this.renderer.listen('window', 'popstate', (event) => {
-            console.log('popstate');
-            this.navigation.back();
-        });
+    this.renderer = this.rendererFactory2.createRenderer(null, null);
+    this.renderer.listen('window', 'popstate', (event) => {
+      console.log('popstate');
+      this.navigation.back();
+    });
 
-        this.renderer.listen('document','unload',(event)=>{
-          console.log(event);
-        });
+    this.renderer.listen('window', 'keyup', (event) => {
+      console.log(event);
+      if (event.code === 'Escape') {
+        this.navigation.back();
+      }
+    });
 
-        this.renderer.listen('window', 'keyup', (event) => {
-            console.log(event);
-            if (event.code === 'Escape') {
-                this.navigation.back();
-            }
-        });
-
-    }
+  }
 
 
 }
