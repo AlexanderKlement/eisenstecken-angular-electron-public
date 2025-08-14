89c8bdff2452319011a92193e638d98d8321cbec
Elias Ebner <eliase229@gmail.com>
2025-08-14 09:48:35 +0200

    should work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,208 +1,239 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {APP_INITIALIZER, ErrorHandler, Injectable, LOCALE_ID, NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule, HttpClient, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
-import {AppRoutingModule} from './app-routing.module';
-import {RouteReuseStrategy} from '@angular/router';
-import {AppRouterOutletDirective} from './router-outlet';
+import { BrowserModule } from "@angular/platform-browser";
+import {
+  APP_INITIALIZER,
+  ErrorHandler,
+  Injectable,
+  LOCALE_ID,
+  NgModule,
+} from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  HttpClientModule,
+  HttpClient,
+  HTTP_INTERCEPTORS,
+} from "@angular/common/http";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { RouteReuseStrategy } from "@angular/router";
+import { AppRouterOutletDirective } from "./router-outlet";
 
 // NG Translate
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {ApiModule, Configuration, ConfigurationParameters} from 'eisenstecken-openapi-angular-library';
-import {HomeModule} from './home/home.module';
-import {APP_CONFIG} from 'environments/environment';
-
-import {AppComponent} from './app.component';
-import {AuthService} from './shared/services/auth.service';
-import {AccessGuard} from './shared/services/access-guard.service';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {ChatService} from './home/chat/chat.service';
-import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {JobModule} from './job/job.module';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
-import {CommonModule, CurrencyPipe, DatePipe, registerLocaleData} from '@angular/common';
-import {SettingsModule} from './settings/settings.module';
-import {OrderModule} from './order/order.module';
-import {LoginModule} from './login/login.module';
-import {Router} from '@angular/router';
-import localeDe from '@angular/common/locales/de';
-import localeDeExtra from '@angular/common/locales/extra/de';
-import * as Sentry from '@sentry/angular';
-import {ClientModule} from './client/client.module';
-import {UserModule} from './user/user.module';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {InvoiceModule} from './invoice/invoice.module';
-import {SupplierModule} from './supplier/supplier.module';
-import {getGermanPaginatorIntl} from './shared/components/table-builder/table-builder.datasource';
-import {RecalculationModule} from './recalculation/recalculation.module';
-import {EmployeeModule} from './employee/employee.module';
-import {GlobalHttpInterceptorService} from './global-http-inceptor.service';
-import {DeliveryNoteModule} from './delivery-note/delivery-note.module';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {DebugModule} from './debug/debug.module';
-import {LocalConfigRenderer} from './LocalConfigRenderer';
-import {CustomReuseStrategy} from './reuse-strategy';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import {
-    CalendarDateFormatter,
-    CalendarModule,
-    CalendarNativeDateFormatter,
-    DateAdapter,
-    DateFormatterParams
-} from 'angular-calendar';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-import {NgxMatDatetimePickerModule, NgxMatNativeDateModule} from '@angular-material-components/datetime-picker';
-import {EventCalendarModule} from './calendar/event-calendar.module';
-import {MobileAppModule} from './mobile-app/mobile-app.module';
-import {ServiceModule} from './service/service.module';
-import {BackButtonDisableModule} from 'angular-disable-browser-back-button';
-import {PhoneBookModule} from './phone-book/phone-book.module';
-// AoT requires an exported function for factories
-const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader => new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  ApiModule,
+  Configuration,
+  ConfigurationParameters,
+} from "eisenstecken-openapi-angular-library";
+import { HomeModule } from "./home/home.module";
+import { APP_CONFIG } from "environments/environment";
 
-registerLocaleData(localeDe, 'de-DE', localeDeExtra);
+import { AppComponent } from "./app.component";
+import { AuthService } from "./shared/services/auth.service";
+import { AccessGuard } from "./shared/services/access-guard.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ChatService } from "./home/chat/chat.service";
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import {
+  MatPaginatorIntl,
+  MatPaginatorModule,
+} from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { JobModule } from "./job/job.module";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MAT_DATE_LOCALE, MatNativeDateModule } from "@angular/material/core";
+import {
+  CommonModule,
+  CurrencyPipe,
+  DatePipe,
+  registerLocaleData,
+} from "@angular/common";
+import { SettingsModule } from "./settings/settings.module";
+import { OrderModule } from "./order/order.module";
+import { LoginModule } from "./login/login.module";
+import { Router } from "@angular/router";
+import localeDe from "@angular/common/locales/de";
+import localeDeExtra from "@angular/common/locales/extra/de";
+import * as Sentry from "@sentry/angular";
+import { ClientModule } from "./client/client.module";
+import { UserModule } from "./user/user.module";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { InvoiceModule } from "./invoice/invoice.module";
+import { SupplierModule } from "./supplier/supplier.module";
+import { getGermanPaginatorIntl } from "./shared/components/table-builder/table-builder.datasource";
+import { RecalculationModule } from "./recalculation/recalculation.module";
+import { EmployeeModule } from "./employee/employee.module";
+import { GlobalHttpInterceptorService } from "./global-http-inceptor.service";
+import { DeliveryNoteModule } from "./delivery-note/delivery-note.module";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import { DebugModule } from "./debug/debug.module";
+import { LocalConfigRenderer } from "./LocalConfigRenderer";
+import { CustomReuseStrategy } from "./reuse-strategy";
+import {
+  CalendarDateFormatter,
+  CalendarModule,
+  CalendarNativeDateFormatter,
+  DateAdapter,
+  DateFormatterParams,
+} from "angular-calendar";
+import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+} from "@angular-material-components/datetime-picker";
+import { EventCalendarModule } from "./calendar/event-calendar.module";
+import { MobileAppModule } from "./mobile-app/mobile-app.module";
+import { ServiceModule } from "./service/service.module";
+import { BackButtonDisableModule } from "angular-disable-browser-back-button";
+import { PhoneBookModule } from "./phone-book/phone-book.module";
+// AoT requires an exported function for factories
+const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>
+  new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+
+registerLocaleData(localeDe, "de-DE", localeDeExtra);
 
 export function apiConfigFactory(): Configuration {
-    const params: ConfigurationParameters = {
-        basePath: LocalConfigRenderer.getInstance().getApi(),
-    };
-    return new Configuration(params);
+  const params: ConfigurationParameters = {
+    basePath: LocalConfigRenderer.getInstance().getApi(),
+  };
+  return new Configuration(params);
 }
 
 @Injectable()
 class CustomDateFormatter extends CalendarNativeDateFormatter {
-
-    public dayViewHour({date, locale}: DateFormatterParams): string {
-        // change this to return a different date format
-        return new Intl.DateTimeFormat(locale, {hour: 'numeric'}).format(date);
-    }
-
+  public dayViewHour({ date, locale }: DateFormatterParams): string {
+    // change this to return a different date format
+    return new Intl.DateTimeFormat(locale, { hour: "numeric" }).format(date);
+  }
 }
 
 @NgModule({
-    declarations: [AppComponent, AppRouterOutletDirective],
-    imports: [
-        CommonModule,
-        BrowserModule,
-        BrowserAnimationsModule,
-        CalendarModule.forRoot({
-            provide: DateAdapter,
-            useFactory: adapterFactory,
+  declarations: [AppComponent, AppRouterOutletDirective],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    FormsModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    JobModule,
+    BackButtonDisableModule.forRoot(),
+    ClientModule,
+    UserModule,
+    SettingsModule,
+    SupplierModule,
+    OrderModule,
+    LoginModule,
+    MatBottomSheetModule,
+    InvoiceModule,
+    EmployeeModule,
+    EventCalendarModule,
+    NgxMatNativeDateModule,
+    NgxMatDatetimePickerModule,
+    MobileAppModule,
+    RecalculationModule,
+    ApiModule.forRoot(apiConfigFactory),
+    AppRoutingModule,
+    FlexLayoutModule,
+    DeliveryNoteModule,
+    DebugModule,
+    ServiceModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    PhoneBookModule,
+    MatSnackBarModule,
+    ...(APP_CONFIG.production
+      ? [
+        ServiceWorkerModule.register("ngsw-worker.js", {
+          enabled: APP_CONFIG.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: "registerWhenStable:30000",
         }),
-        FormsModule,
-        HttpClientModule,
-        CoreModule,
-        SharedModule,
-        HomeModule,
-        JobModule,
-        BackButtonDisableModule.forRoot(),
-        ClientModule,
-        UserModule,
-        SettingsModule,
-        SupplierModule,
-        OrderModule,
-        LoginModule,
-        MatBottomSheetModule,
-        InvoiceModule,
-        EmployeeModule,
-        EventCalendarModule,
-        NgxMatNativeDateModule,
-        NgxMatDatetimePickerModule,
-        MobileAppModule,
-        RecalculationModule,
-        ApiModule.forRoot(apiConfigFactory),
-        AppRoutingModule,
-        FlexLayoutModule,
-        DeliveryNoteModule,
-        DebugModule,
-        ServiceModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: httpLoaderFactory,
-                deps: [HttpClient]
-            }
+      ]
+      : []),
+    MatIconModule,
+  ],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: false,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => { },
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+    {
+      provide: Configuration,
+      useFactory: (authService: AuthService) =>
+        new Configuration({
+          accessToken: authService.getToken.bind(authService),
+          basePath: LocalConfigRenderer.getInstance().getApi(),
         }),
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        MatCheckboxModule,
-        MatInputModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatProgressSpinnerModule,
-        MatSelectModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatNativeDateModule,
-        PhoneBookModule,
-        MatSnackBarModule,
-        ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: APP_CONFIG.production,
-            // Register the ServiceWorker as soon as the app is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000'
-        }),
-        MatIconModule
-    ],
-    providers:
-        [
-            {
-                provide: ErrorHandler,
-                useValue: Sentry.createErrorHandler({
-                    showDialog: false,
-                }),
-            }, {
-            provide: Sentry.TraceService,
-            deps: [Router],
-
-        }, {
-            provide: APP_INITIALIZER,
-            useFactory: () => () => {
-            },
-            deps: [Sentry.TraceService],
-            multi: true,
-        }, {
-            provide: Configuration,
-            useFactory: (authService: AuthService) => new Configuration(
-                {
-                    accessToken: authService.getToken.bind(authService),
-                    basePath: LocalConfigRenderer.getInstance().getApi(),
-                }
-            ),
-            deps: [AuthService],
-            multi: false
-        },
-            AccessGuard,
-            ChatService,
-            CurrencyPipe,
-            DatePipe,
-            {
-                provide: MAT_DATE_LOCALE, useValue: 'de-DE'
-            },
-            {
-                provide: LOCALE_ID,
-                useValue: 'de-DE' // 'de-DE' for Germany, 'fr-FR' for France ...
-            },
-            {provide: MatPaginatorIntl, useValue: getGermanPaginatorIntl()},
-            {provide: HTTP_INTERCEPTORS, useClass: GlobalHttpInterceptorService, multi: true},
-            {provide: RouteReuseStrategy, useClass: CustomReuseStrategy},
-            {provide: CalendarDateFormatter, useClass: CustomDateFormatter}
-        ],
-    bootstrap: [AppComponent]
+      deps: [AuthService],
+      multi: false,
+    },
+    AccessGuard,
+    ChatService,
+    CurrencyPipe,
+    DatePipe,
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: "de-DE",
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: "de-DE", // 'de-DE' for Germany, 'fr-FR' for France ...
+    },
+    { provide: MatPaginatorIntl, useValue: getGermanPaginatorIntl() },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GlobalHttpInterceptorService,
+      multi: true,
+    },
+    { provide: RouteReuseStrategy, useClass: CustomReuseStrategy },
+    { provide: CalendarDateFormatter, useClass: CustomDateFormatter },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,33 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import 'zone.js';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import * as Sentry from '@sentry/angular';
-import {Integrations} from '@sentry/tracing';
+import { Integrations } from '@sentry/tracing';
 import * as moment from 'moment';
-import {AppModule} from './app/app.module';
-import {APP_CONFIG} from './environments/environment';
-import {LocalConfigRenderer} from './app/LocalConfigRenderer';
-
+import { AppModule } from './app/app.module';
+import { APP_CONFIG } from './environments/environment';
+import { LocalConfigRenderer } from './app/LocalConfigRenderer';
 
 Sentry.init({
-    dsn: 'https://739b39d0b92749a485e48a80da87816e@sentry.kivi.bz.it/26',
-    integrations: [
-        new Integrations.BrowserTracing(),
-    ],
+  dsn: 'https://739b39d0b92749a485e48a80da87816e@sentry.kivi.bz.it/26',
+  integrations: [new Integrations.BrowserTracing()],
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.5,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
 });
 
 LocalConfigRenderer.getInstance().init();
 
-
 moment.locale('de');
 
 if (APP_CONFIG.production) {
-    enableProdMode();
+  enableProdMode();
 }
 
-
 platformBrowserDynamic()
-    .bootstrapModule(AppModule, {
-        preserveWhitespaces: false
-    })
-    .catch(err => console.error(err));
-
-
-
+  .bootstrapModule(AppModule, {
+    preserveWhitespaces: false,
+  })
+  .catch((err) => console.error(err));
