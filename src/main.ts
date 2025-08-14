1aa2d166278d4808b0570f39fac7e037342e5d95
Elias Ebner <eliase229@gmail.com>
2025-08-14 11:52:57 +0200

    kalle help pls

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,6 @@ import { LocalConfigRenderer } from './app/LocalConfigRenderer';
 Sentry.init({
   dsn: 'https://739b39d0b92749a485e48a80da87816e@sentry.kivi.bz.it/26',
   integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 0.5,
 });
 
