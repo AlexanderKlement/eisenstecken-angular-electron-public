0b40224c82b7b3e564be7dad33efb77b61578209
Alexander Klement <kalle@kivi.bz.it>
2025-08-19 15:40:36 +0200

    remove unused `additionalWorkload.service.ts`, add several new autogenerated models

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,7 @@
-export * from './additionalWorkload.service';
-import { AdditionalWorkloadService } from './additionalWorkload.service';
 export * from './default.service';
 import { DefaultService } from './default.service';
 export * from './health.service';
 import { HealthService } from './health.service';
 export * from './scheduler.service';
 import { SchedulerService } from './scheduler.service';
-export const APIS = [AdditionalWorkloadService, DefaultService, HealthService, SchedulerService];
+export const APIS = [DefaultService, HealthService, SchedulerService];
