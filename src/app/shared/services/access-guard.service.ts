adfea158ff9591fab7203ff4203cba2c99c19660
Alexander Klement <kalle@kivi.bz.it>
2025-11-07 12:22:33 +0100

    fixed a lot of errors by using the angular styles

--- src/app/shared/services/access-guard.service.ts
+++ src/app/shared/services/access-guard.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, Inject, PLATFORM_ID } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { AuthService } from "./auth.service";
-import { ElectronService } from "../../core/services";
-import { isPlatformBrowser } from "@angular/common";
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from './auth.service';
+import { ElectronService } from '../../core/services';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class AccessGuard implements CanActivate {
 
   private readonly limitAccessHosts: string[] = [
-    "stunden.eisenstecken.kivi.bz.it",
-    "timedev.app.eisenstecken.it",
-    "time.app.eisenstecken.it",
+    'stunden.eisenstecken.kivi.bz.it',
+    'timedev.app.eisenstecken.it',
+    'time.app.eisenstecken.it',
   ];
 
   constructor(
@@ -18,18 +18,19 @@ export class AccessGuard implements CanActivate {
     private router: Router,
     private electron: ElectronService,
     @Inject(PLATFORM_ID) private platformId: Object,
-  ) {}
+  ) {
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    _: RouterStateSnapshot,
   ): boolean | UrlTree {
     // default: requires login unless explicitly set to false
     const requiresLogin = route.data?.requiresLogin !== false;
 
     if (requiresLogin && !this.authService.isLoggedIn()) {
-      console.warn("Not logged in!");
-      return this.router.createUrlTree(["login"]);
+      console.warn('Not logged in!');
+      return this.router.createUrlTree(['login']);
     }
 
     return this.redirectWorkHours(route);
@@ -43,9 +44,9 @@ export class AccessGuard implements CanActivate {
     if (!this.limitAccessHosts.includes(hostname)) return true;
 
     // allow /mobile routes, or Electron app
-    const firstSeg = route.url[0]?.path ?? "";
-    if (firstSeg.startsWith("mobile") || this.electron.isElectron) return true;
+    const firstSeg = route.url[0]?.path ?? '';
+    if (firstSeg.startsWith('mobile') || this.electron.isElectron) return true;
 
-    return this.router.createUrlTree(["mobile"]);
+    return this.router.createUrlTree(['mobile']);
   }
 }
