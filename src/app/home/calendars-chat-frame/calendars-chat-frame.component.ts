ef3ff4aad461b9aa1d8ebb5a164ff6f97b72dfd2
Elias Ebner <eliase229@gmail.com>
2025-12-10 18:41:01 +0100

    chat and calendar box is now smaller but chat window is bigger

--- src/app/home/calendars-chat-frame/calendars-chat-frame.component.ts
+++ src/app/home/calendars-chat-frame/calendars-chat-frame.component.ts
@@ -1,41 +1,60 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
-import { first, tap } from 'rxjs/operators';
-import { ChatService } from '../chat/chat.service';
-import { DefaultService, Calendar } from '../../../api/openapi';
-import { LoadingComponent } from '../../shared/components/loading/loading.component';
-import { MatTabGroup, MatTab } from '@angular/material/tabs';
-import { FlexModule, DefaultLayoutDirective, DefaultLayoutAlignDirective } from 'ng-flex-layout';
-import { SimpleCalendarComponent } from '../../shared/components/calendar/simple-calendar.component';
-import { EventCalendarComponent } from '../event-calendar/event-calendar.component';
-import { ChatComponent } from '../chat/chat.component';
-import { AsyncPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { Observable } from "rxjs";
+import { first, tap } from "rxjs/operators";
+import { ChatService } from "../chat/chat.service";
+import { DefaultService, Calendar } from "../../../api/openapi";
+import { LoadingComponent } from "../../shared/components/loading/loading.component";
+import { MatTabGroup, MatTab } from "@angular/material/tabs";
+import {
+  FlexModule,
+  DefaultLayoutDirective,
+  DefaultLayoutAlignDirective,
+} from "ng-flex-layout";
+import { SimpleCalendarComponent } from "../../shared/components/calendar/simple-calendar.component";
+import { EventCalendarComponent } from "../event-calendar/event-calendar.component";
+import { ChatComponent } from "../chat/chat.component";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
-    selector: 'app-calendars-frame',
-    templateUrl: './calendars-chat-frame.component.html',
-    styleUrls: ['./calendars-chat-frame.component.scss'],
-    imports: [LoadingComponent, MatTabGroup, FlexModule, MatTab, DefaultLayoutDirective, DefaultLayoutAlignDirective, SimpleCalendarComponent, EventCalendarComponent, ChatComponent, AsyncPipe]
+  selector: "app-calendars-frame",
+  templateUrl: "./calendars-chat-frame.component.html",
+  styleUrls: ["./calendars-chat-frame.component.scss"],
+  imports: [
+    LoadingComponent,
+    MatTabGroup,
+    FlexModule,
+    MatTab,
+    DefaultLayoutDirective,
+    DefaultLayoutAlignDirective,
+    SimpleCalendarComponent,
+    EventCalendarComponent,
+    ChatComponent,
+    AsyncPipe,
+  ],
 })
 export class CalendarsChatFrameComponent implements OnInit, OnDestroy {
-
-  @ViewChild('chatTab') chatTab;
+  @ViewChild("chatTab") chatTab;
 
   calendars$: Observable<Calendar[]>;
   loading = true;
-  chatTabName = '  Chat';
+  chatTabName = "  Chat";
 
   checkIfUnreadMessagesInterval: NodeJS.Timeout;
   secondsCheckIfUnreadMessages = 10;
-  eventTabName = ' Betriebskalender';
+  eventTabName = " Betriebskalender";
 
-  constructor(private api: DefaultService, private chatService: ChatService) {
-  }
+  constructor(
+    private api: DefaultService,
+    private chatService: ChatService,
+  ) {}
 
   ngOnInit(): void {
-    this.calendars$ = this.api.readCalendarsCalendarGet().pipe(first(), tap(() => {
-      this.loading = false;
-    }));
+    this.calendars$ = this.api.readCalendarsCalendarGet().pipe(
+      first(),
+      tap(() => {
+        this.loading = false;
+      }),
+    );
     /*
     this.checkIfUnreadMessagesInterval = setInterval(() => {
       this.resetUnreadChatMessageCountIfActive();
@@ -53,6 +72,4 @@ export class CalendarsChatFrameComponent implements OnInit, OnDestroy {
       this.chatService.resetUnreadMessageCount();
     }
   }
-
-
 }
